ae3def98dfac00d93b081224dcc5214c96db6397
Dan Connolly <connolly@agoric.com>
2022-02-08 15:55:58 -0600

    fixup(vats): export fundAMM from behaviors

--- packages/vats/src/core/behaviors.js
+++ packages/vats/src/core/behaviors.js
@@ -3,4 +3,6 @@ export * from './basic-behaviors.js';
 export * from './chain-behaviors.js';
 export * from '@agoric/run-protocol/src/econ-behaviors.js';
 export { installOnChain as installPegasusOnChain } from '@agoric/pegasus/src/install-on-chain.js';
+export { fundAMM } from '../demoIssuers.js';
+
 // We exclude sim-behaviors.js from this list because it should not be used in production.
